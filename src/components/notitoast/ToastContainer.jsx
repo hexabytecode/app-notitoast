1cd7f09dab4b7d66450b14cdb32a34025ffff41c
Aditya Uphade <hexabytecode@gmail.com>
2025-03-04 10:22:28 +0530

    fix: added reusable utilFunction

--- src/components/notitoast/ToastContainer.jsx
+++ src/components/notitoast/ToastContainer.jsx
@@ -1,16 +1,5 @@
 import Toast from "./Toast";
-
-const getPositionClass = (vertical, horizontal) => {
-  const verticalClass = vertical === "top" ? "top-4" : "bottom-4";
-  const horizontalClass =
-    horizontal === "left"
-      ? "left-4"
-      : horizontal === "right"
-      ? "right-4"
-      : "left-1/2 transform -translate-x-1/2";
-
-  return `${verticalClass} ${horizontalClass}`;
-};
+import { getPositionClass } from "./toastUtils";
 
 const ToastContainer = ({ toasts, removeToast }) => {
   const groupedToasts = toasts.reduce((groups, toast) => {
--- src/components/notitoast/toastUtils.js
+++ src/components/notitoast/toastUtils.js
@@ -1,2 +1,13 @@
 export const generateId = () => Date.now();
 
+export const getPositionClass = (vertical, horizontal) => {
+  const verticalClass = vertical === "top" ? "top-4" : "bottom-4";
+  const horizontalClass =
+    horizontal === "left"
+      ? "left-4"
+      : horizontal === "right"
+      ? "right-4"
+      : "left-1/2 transform -translate-x-1/2";
+
+  return `${verticalClass} ${horizontalClass}`;
+};
